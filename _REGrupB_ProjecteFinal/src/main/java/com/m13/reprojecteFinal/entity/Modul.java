130db32302c2e41d34521b7bc499d2b13ebe5600
MarcoLaurenao <mlaurean19@jaumebalmes.net>
2022-12-05 17:30:36 +0100

    UnitatFormativa + Moduls

--- _REGrupB_ProjecteFinal/src/main/java/com/m13/reprojecteFinal/entity/Modul.java
+++ _REGrupB_ProjecteFinal/src/main/java/com/m13/reprojecteFinal/entity/Modul.java
@@ -1,9 +1,11 @@
 package com.m13.reprojecteFinal.entity;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Modul {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
